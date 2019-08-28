5ec9f4bbf235d61fc803e6352d58a4035431d5a2
DenislavVelichkov <denislav.velichkov@gmail.com>
2019-08-28 08:22:25 +0300

    New Project structures added

--- Hibernate-Projects/03-09. LECTURES/src/main/java/Students_Homeworks/_01_DB_APPS_Introduction/Student_1/prob7/Minion.java
+++
@@ -1,9 +0,0 @@
-package Students_Homeworks._01_DB_APPS_Introduction.Student_1.prob7;
-
-public class Minion {
-    public String name;
-
-    public Minion(String name){
-        this.name = name;
-    }
-}
--- Hibernate-Projects/03-09. LECTURES/src/main/java/Students_Homeworks/_01_DB_APPS_Introduction/Student_2/Interfaces/Executable.java
+++
@@ -1,5 +0,0 @@
-package Students_Homeworks._01_DB_APPS_Introduction.Student_2.Interfaces;
-
-public interface Executable {
-    void execute();
-}
